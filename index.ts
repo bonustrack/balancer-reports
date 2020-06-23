79dac4264889815fb8cc0eea871b4a22f97e9f32
adcpm <fabien@bonustrack.co>
2020-06-23 18:52:40 +0700

    Add express server

---
+++ index.ts
@@ -0,0 +1,12 @@
+import serveStatic from 'serve-static';
+import bodyParser from 'body-parser';
+
+export default (app) => {
+  app.use(bodyParser.json({ limit: '20mb' }));
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+  // @ts-ignore
+  app.use(serveStatic(`${__dirname}/dist`));
+  // @ts-ignore
+  app.get('/', (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
+  app.get('*', (req, res) => res.redirect('/'));
+}
